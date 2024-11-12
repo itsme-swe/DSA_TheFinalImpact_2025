aa58c27b19a1c9f45e89a11bb7ef3baa842dcadc
itsme-swe <me.harshmehra07@gmail.com>
2024-11-13 05:27:43 +0530

    New repo of DSA

---
+++ src/patternPrinting/numberTriangle.java
@@ -0,0 +1,29 @@
+package patternPrinting;
+
+import java.util.Scanner;
+
+public class numberTriangle {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+Output:
+1
+1 2
+1 2 3
+1 2 3 4
+
+*/
