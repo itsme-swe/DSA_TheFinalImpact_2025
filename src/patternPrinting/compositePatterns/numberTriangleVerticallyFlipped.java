fbea981749523377eeef3de32f9ad3c686155ebe
itsme-swe <me.harshmehra07@gmail.com>
2024-11-13 18:15:40 +0530

    New repo of DSA

---
+++ src/patternPrinting/compositePatterns/numberTriangleVerticallyFlipped.java
@@ -0,0 +1,34 @@
+package patternPrinting.compositePatterns;
+
+import java.util.Scanner;
+
+public class numberTriangleVerticallyFlipped {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i<= n; i++) {
+            for (int j = 1; j <= n-i; j++){
+                System.out.print(" " + " ");
+            }
+            for (int j = 1; j <= i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+
+      1
+    1 2
+  1 2 3
+1 2 3 4
+
+
+* */
