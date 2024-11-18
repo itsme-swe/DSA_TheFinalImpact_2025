f2cc9358df38f4051f68117aa4e6ae5b64804337
itsme-swe <me.harshmehra07@gmail.com>
2024-11-18 10:59:29 +0530

    New repo of DSA

---
+++ src/arrayAndArrayList/arrays/singleDimensionalArrays/questionsOnArrays/dutchFlagAlgo.java
@@ -0,0 +1,11 @@
+/*
+🍁 Sort the array of 0's, 1's and 2's. (Dutch Flag Algorithm)
+* */
+package arrayAndArrayList.arrays.singleDimensionalArrays.questionsOnArrays;
+
+public class dutchFlagAlgo {
+
+    public static void main(String[] args) {
+
+    }
+}
