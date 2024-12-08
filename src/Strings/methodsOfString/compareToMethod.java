a4a95908dba5e197815b30da6a149684f1ee7f3b
itsme-swe <me.harshmehra07@gmail.com>
2024-12-09 05:29:58 +0530

    New repo of DSA

---
+++ src/Strings/methodsOfString/compareToMethod.java
@@ -0,0 +1,12 @@
+package Strings.methodsOfString;
+
+public class compareToMethod {
+
+    public static void main(String[] args) {
+
+         String str1 = "amaze";
+         String str2 = "amit";
+
+        System.out.println(str1.compareTo(str2)); // o/p: -8 bcoz compareTo method returns the difference of ASCII values of characters.
+    }
+}
---
+++ src/Strings/methodsOfString/indexOfMethod.java
@@ -0,0 +1,11 @@
+package Strings.methodsOfString;
+
+public class indexOfMethod {
+
+    public static void main(String[] args) {
+
+        String name = "Harsh";
+
+        System.out.println(name.indexOf('h'));  // return the indexVal of first occurrence of char
+    }
+}
