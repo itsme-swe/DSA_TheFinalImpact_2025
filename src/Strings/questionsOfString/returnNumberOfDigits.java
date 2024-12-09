572fbdeccb83b80a1f2c1e8197f277001b8db0af
itsme-swe <me.harshmehra07@gmail.com>
2024-12-09 10:51:25 +0530

    New repo of DSA

---
+++ src/Strings/questionsOfString/returnNumberOfDigits.java
@@ -0,0 +1,15 @@
+package Strings.questionsOfString;
+
+public class returnNumberOfDigits {
+
+    public static void main(String[] args) {
+
+        int num = 1010506;
+
+        String str = "";
+
+        str += num;
+
+        System.out.println(str.length());   // o/p: 7
+    }
+}
