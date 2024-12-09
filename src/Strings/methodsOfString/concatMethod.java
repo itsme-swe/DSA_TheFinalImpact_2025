af057fee838bdec2f29ab81db46949bc5282ae25
itsme-swe <me.harshmehra07@gmail.com>
2024-12-09 05:48:50 +0530

    New repo of DSA

---
+++ src/Strings/methodsOfString/concatMethod.java
@@ -0,0 +1,14 @@
+package Strings.methodsOfString;
+
+public class concatMethod {
+
+    public static void main(String[] args) {
+
+        String s1 = "Harsh";
+
+        String s2 = " Mehra";
+
+        System.out.println(s1.concat(s2));  // o/p: Harsh Mehra
+
+    }
+}
