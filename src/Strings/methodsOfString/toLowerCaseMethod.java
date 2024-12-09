96017e9a528acaba97a3a60812d80684ec4bb364
itsme-swe <me.harshmehra07@gmail.com>
2024-12-09 05:46:57 +0530

    New repo of DSA

---
+++ src/Strings/methodsOfString/toLowerCaseMethod.java
@@ -0,0 +1,11 @@
+package Strings.methodsOfString;
+
+public class toLowerCaseMethod {
+
+    public static void main(String[] args) {
+
+        String carName = "SKODA";
+
+        System.out.println(carName.toLowerCase());
+    }
+}
---
+++ src/Strings/methodsOfString/toUpperCaseMethod.java
@@ -0,0 +1,11 @@
+package Strings.methodsOfString;
+
+public class toUpperCaseMethod {
+
+    public static void main(String[] args) {
+
+        String name = "harsh";
+
+        System.out.println(name.toUpperCase());
+    }
+}
