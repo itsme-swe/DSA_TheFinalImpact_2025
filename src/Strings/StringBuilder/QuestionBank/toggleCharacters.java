0a2bffccd888860610d7a794a073f1697a1877a4
itsme-swe <me.harshmehra07@gmail.com>
2024-12-10 08:18:11 +0530

    New repo of DSA

---
+++ src/Strings/StringBuilder/QuestionBank/toggleCharacters.java
@@ -0,0 +1,39 @@
+/*
+💥 Toggle all the characters of the string.(Replace small case with capital case and vice vera)
+
+*/
+
+package Strings.StringBuilder.QuestionBank;
+
+public class toggleCharacters {
+
+    public static void main(String[] args) {
+
+        String s = "sKoDa sLAviA";
+
+        System.out.println(s);
+
+        StringBuilder sb = new StringBuilder(s);
+
+        int n = sb.length();
+
+        for (int i = 0; i < n; i++) {
+
+            char ch = sb.charAt(i); // storing each character of string in ch
+
+            int ascii = (int)ch;    // Then typecasting each char into int
+
+            // Checking if each 'ch' is a capital letter then converting into a small letter. To make it small, add 32 into ascii
+            if(ascii >= 65 && ascii <= 90) {
+                ascii += 32;
+                ch = (char)ascii;
+                sb.setCharAt(i,ch);
+            } else if (ascii >= 97 && ascii <= 122) {   // Same to convert from small to capital
+                ascii -= 32;
+                ch = (char)ascii;
+                sb.setCharAt(i,ch);
+            }
+        }
+        System.out.println(sb);
+    }
+}
